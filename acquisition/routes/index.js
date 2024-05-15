cbecce66b837c5d722acb05cd59e19694cdc806a
Nokyami <la228834@student.helha.be>
2024-05-15 10:32:37 +0200

    update_tty

--- acquisition/routes/index.js
+++ acquisition/routes/index.js
@@ -5,7 +5,7 @@ const prisma = new PrismaClient()
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline')
 
-const port = "ttyUSB0"
+const port = "/dev/ttyUSB0"
 
 const arduino = new SerialPort({
   path: port,
